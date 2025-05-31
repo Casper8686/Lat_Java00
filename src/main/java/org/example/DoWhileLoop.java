afc3ba20cc1733b21cd1c2145b2f374575703b7e
Casper8686 <caspersmart721@gmail.com>
2025-05-31 09:53:22 +0700

    Lat00-tambah ForLoop tambah Dowhile

---
+++ src/main/java/org/example/DoWhileLoop.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        var counter = 100;
+        do{
+        System.out.println("perulangan" + counter);
+        counter++;
+        }while ( counter<= 10);
+    }
+}
